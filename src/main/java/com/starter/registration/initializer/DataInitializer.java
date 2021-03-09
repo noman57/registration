4f0572eff5c21eb2177976facb384fe55ab81fe7
noman57 <noman7119@yahoo.com>
2021-03-09 10:18:28 +0100

    added data initializer

---
+++ src/main/java/com/starter/registration/initializer/DataInitializer.java
@@ -0,0 +1,29 @@
+package com.starter.registration.initializer;
+
+import com.starter.registration.dto.UserCreateDTO;
+import com.starter.registration.enumuration.Breed;
+import com.starter.registration.service.UserService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class DataInitializer {
+
+    private UserService userService;
+
+    public DataInitializer(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostConstruct
+    public void init() {
+        UserCreateDTO userCreateDTO = new UserCreateDTO();
+        userCreateDTO.setLastName("lname");
+        userCreateDTO.setFirstName("fname");
+        userCreateDTO.setEmailId("test@test.com");
+        userCreateDTO.setPassword("password");
+        userCreateDTO.setPreferredPet(Breed.PUG);
+        userService.createUser(userCreateDTO);
+    }
+}
